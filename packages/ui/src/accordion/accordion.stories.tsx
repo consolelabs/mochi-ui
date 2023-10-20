e209d254c07c1cdff8f0b056cfc08e44c014bdcc
tonible14012002 <namanhble14012002@gmail.com>
2023-10-20 11:36:09 +0700

    fix: add accordion style prop

--- packages/ui/src/accordion/accordion.stories.tsx
+++ packages/ui/src/accordion/accordion.stories.tsx
@@ -15,7 +15,7 @@ const meta: Meta<typeof Accordion> = {
   args: {
     type: 'single',
     collapsible: true,
-    className: 'ui-px-3 ui-rounded-lg ui-shadow-md ui-divide-y ui-max-w-md',
+    className: 'ui-max-w-md',
   },
   argTypes: {
     type: {
@@ -30,43 +30,44 @@ export default meta
 type Story = StoryObj<typeof Accordion>
 
 export const Default: Story = {
-  render: (props) => {
-    return (
-      <Accordion {...props}>
-        <AccordionItem value="2">
-          <AccordionTrigger className="ui-gap-3">
-            <IconQuests fontSize={20} />
-            Server Management
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes, you can try us for free for 30 days. Our friendly team will
-            work with you to get you up and running as soon as possible.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="3">
-          <AccordionTrigger className="ui-gap-3">
-            <IconGame fontSize={20} />
-            Game Store
-          </AccordionTrigger>
-          <AccordionContent>
-            <ul className="ui-space-y-3">
-              <li>Pod Town</li>
-              <li>Triple Pod</li>
-              <li>Lottery Tower</li>
-            </ul>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="4">
-          <AccordionTrigger>Servers</AccordionTrigger>
-          <AccordionContent>
-            <ul className="ui-space-y-3">
-              <li>Server 0</li>
-              <li>Server 1</li>
-              <li>Server 2</li>
-            </ul>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    )
-  },
+    render: (props) => {
+        return (
+            <Accordion {...props}>
+                <AccordionItem value="2">
+                    <AccordionTrigger leftIcon={<IconQuests/>}> 
+                        Server Management
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        Yes, you can try us for free for 30 days. Our friendly team will work with you to get you up and running as soon as possible.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="3">
+                    <AccordionTrigger leftIcon={<IconGame/>}>
+                        Game Store
+                    </AccordionTrigger>
+                    <AccordionContent hasPadding>
+                        PodTown
+                    </AccordionContent>
+                    <AccordionContent hasPadding>
+                        Tripple Pod
+                    </AccordionContent>
+                    <AccordionContent hasPadding>
+                        Lottery Tower
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="4">
+                    <AccordionTrigger>
+                        Servers
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        <ul className="ui-space-y-3">
+                            <li>Server 0</li>
+                            <li>Server 1</li>
+                            <li>Server 2</li>
+                        </ul>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        )
+    }
 }
--- packages/ui/src/accordion/accordion.tsx
+++ packages/ui/src/accordion/accordion.tsx
@@ -1,72 +1,123 @@
-import * as AccordionPrimitive from '@radix-ui/react-accordion'
-import type { ElementRef, ComponentPropsWithoutRef } from 'react'
-import { forwardRef } from 'react'
-import clsx from 'clsx'
-import IconChevron from '../icons/components/icon-chevron'
+import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import type { ElementRef, ComponentPropsWithoutRef, ReactNode } from "react";
+import { forwardRef } from "react"
+import clsx from "clsx";
+import IconChevron from "../icons/components/icon-chevron";
 
-const Accordion = AccordionPrimitive.Root
+const Accordion = forwardRef<
+  ElementRef<typeof AccordionPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AccordionPrimitive.Root>
+>(({className, ...props}, ref) => (
+  <AccordionPrimitive.Root
+    className={clsx(
+      "ui-p-2 ui-space-y-1 ui-rounded-lg ui-shadow-md",
+      className
+    )}
+    ref={ref}
+    {...props}
+  />
+))
+
+Accordion.displayName = AccordionPrimitive.Root.displayName
 
 const AccordionItem = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Item>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->((props, ref) => <AccordionPrimitive.Item ref={ref} {...props} />)
-AccordionItem.displayName = 'AccordionItem'
+    ElementRef<typeof AccordionPrimitive.Item>,
+    ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({className, ...props}, ref) => (
+    <AccordionPrimitive.Item
+      className={clsx(
+        "",
+        className
+      )}
+        ref={ref}
+        {...props}
+    />
+))
+AccordionItem.displayName = "AccordionItem"
 
 interface AccordionTriggerProps
   extends ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> {
   className?: string
-  showIcon?: boolean
+  rightIcon?: ReactNode
   wrapperClassName?: string
+  hasPadding?: boolean
+  leftIcon?: ReactNode
 }
 
 const AccordionTrigger = forwardRef<
   ElementRef<typeof AccordionPrimitive.Trigger>,
   AccordionTriggerProps
->(
-  (
-    { children, className, wrapperClassName, showIcon = true, ...props },
-    ref,
-  ) => (
+>((props , ref) => {
+
+  const { children, className, wrapperClassName, rightIcon, leftIcon, ...restProps} = props
+
+  const iconStyle = "ui-text-neutral-500 ui-shrink-0 ui-transition-transform ui-duration-200 ui-text-base"
+  const renderRightIcon = (
+    <span className={iconStyle} id={!rightIcon ? "ui-chevron": undefined}>
+      {!rightIcon ? (
+        <IconChevron />
+      ): rightIcon}
+    </span>
+  )
+
+  const renderLeftIcon = leftIcon ? (
+    <span className="ui-text-xl">
+      {leftIcon}
+    </span>
+  ): null
+
+  return (
     <AccordionPrimitive.Header className="ui-flex">
       <AccordionPrimitive.Trigger
         className={clsx(
-          'ui-flex-1 ui-flex ui-font-medium ui-text-sm ui-items-center ui-justify-between [&[data-state=open]>#chevron]:ui-rotate-180 ui-gap-3 ui-py-3',
-          wrapperClassName,
+          "ui-flex-1 ui-flex ui-font-medium ui-text-sm ui-items-center ui-justify-between [&[data-state=open]>#ui-chevron]:ui-rotate-180 ui-gap-4 ui-p-2",
+          wrapperClassName
         )}
         ref={ref}
-        {...props}
+        {...restProps}
       >
-        <span className={clsx('ui-flex ui-flex-1', className)}>{children}</span>
-        {showIcon ? (
-          <IconChevron
-            className="ui-text-base ui-transition-transform ui-duration-200 ui-text-neutral-500 ui-shrink-0"
-            id="chevron"
-          />
-        ) : null}
+        {renderLeftIcon}
+        <span className={clsx("ui-flex ui-flex-1", className)}>
+          {children}
+        </span>
+        {renderRightIcon}
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  ),
-)
+  )
+})
 
 AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
 
 interface AccordionContentProps
   extends ComponentPropsWithoutRef<typeof AccordionPrimitive.Content> {
   wrapperClassName?: string
+  hasPadding?: boolean
 }
 
 const AccordionContent = forwardRef<
   ElementRef<typeof AccordionPrimitive.Content>,
   AccordionContentProps
->(({ children, className, wrapperClassName, ...props }, ref) => (
-  <AccordionPrimitive.Content
-    className={clsx('ui-text-sm', wrapperClassName)}
-    ref={ref}
-    {...props}
-  >
-    <div className={clsx('ui-pb-3 ui-pt-0', className)}>{children}</div>
-  </AccordionPrimitive.Content>
-))
+>((props, ref) => {
+  const { children, className, wrapperClassName, hasPadding=false, ...restProps } = props
+  return (
+    <AccordionPrimitive.Content
+      className={clsx('ui-text-sm', wrapperClassName)}
+      ref={ref}
+      {...restProps}
+    >
+      <div
+        className={clsx(
+          "ui-px-2 ui-py-2", {
+            "ui-px-11": hasPadding
+          },
+          className
+        )}
+      >
+        {children}
+      </div>
+    </AccordionPrimitive.Content>
+  )
+})
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
