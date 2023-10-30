192e5c05ddc581bcb9d67782700f7a4cf61d0055
tonible14012002 <namanhble14012002@gmail.com>
2023-10-30 10:54:45 +0700

    feat: add modal example storybook

--- packages/ui/src/modal/modal.stories.tsx
+++ packages/ui/src/modal/modal.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Modal, ModalContent, ModalDescription, ModalTitle, ModalTrigger } from "./modal";
+import { IconCheckCircled } from "../icons";
 
 const meta: Meta<typeof Modal> = {
     title: 'ui/Modal',
@@ -23,7 +24,7 @@ export const Default: Story = {
                         Open modal
                     </button>
                 </ModalTrigger>
-                <ModalContent className="space-y-2" showCloseBtn>
+                <ModalContent className="space-y-2">
                     <ModalTitle>Hello</ModalTitle>
                     <ModalDescription>
                         This is a modal description. 
@@ -31,6 +32,71 @@ export const Default: Story = {
                         This is a modal description.
                         This is a modal description.
                     </ModalDescription>
+                </ModalContent>
+            </Modal>
+        )
+    }
+}
+
+export const WithCloseButton: Story = {
+    render({modal}) {
+        return (
+            <Modal modal={modal}>
+                <ModalTrigger asChild>
+                    <button type="button">
+                        Open modal
+                    </button>
+                </ModalTrigger>
+                <ModalContent className="space-y-2 max-w-sm" showCloseBtn>
+                    <ModalTitle>Hello</ModalTitle>
+                    <ModalDescription>
+                        This is a modal description. 
+                        This is a modal description.
+                        This is a modal description.
+                        This is a modal description.
+                    </ModalDescription>
+                </ModalContent>
+            </Modal>
+        )
+    }
+}
+
+export const WithConfirm: Story = {
+    render({modal}) {
+        return (
+            <Modal modal={modal}>
+                <ModalTrigger asChild>
+                    <button type="button">
+                        Open modal
+                    </button>
+                </ModalTrigger>
+                <ModalContent className="space-y-2 text-center max-w-sm">
+                    <div className="mb-8 flex flex-col gap-5 items-center">
+                        <IconCheckCircled className="text-4xl"/>
+                        <div>
+                            <ModalTitle className="mb-2">Network Confirmation</ModalTitle>
+                            <ModalDescription>
+                            ETH selected as the transfer network. Few platforms support the ETH network. Please confirm that the receiving platform supports this network. 
+                            </ModalDescription>
+                        </div>
+                    </div>
+                    <div className="flex gap-3">
+                        <ModalTrigger asChild>
+                            <button className="flex-1 border border-neutral-300 py-3 px-6 rounded-lg" type="button">
+                                Cancel
+                            </button>
+                        </ModalTrigger>
+                        <ModalTrigger asChild>
+                            <button 
+                                className="flex-1 bg-primary-700 py-3 px-6 text-white rounded-lg"
+                                // eslint-disable-next-line no-alert -- for demo
+                                onClick={()=> alert("Confirmed")}
+                                type="button"
+                            >
+                                Confirm
+                            </button>
+                        </ModalTrigger>
+                    </div>
                 </ModalContent>
             </Modal>
         )
