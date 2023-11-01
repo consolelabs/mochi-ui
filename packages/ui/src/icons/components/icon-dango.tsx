ec8d0f04b24137ff19a41f5d61085f1567f96d61
TuanD <vincenttuandao@gmail.com>
2023-11-01 10:41:18 +0700

    feat: add icons and a story for displaying icons (#45)

---
+++ packages/ui/src/icons/components/icon-dango.tsx
@@ -0,0 +1,201 @@
+import type { SVGProps } from 'react'
+
+function IconDango(props: SVGProps<SVGSVGElement>) {
+  return <svg
+    height="1em"
+    viewBox="0 0 32 32"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g fill="none">
+      <path
+        d="M6.777 8.238c-.37 0-.74-.14-1.03-.43l-3.32-3.32c-.57-.57-.57-1.49 0-2.06c.57-.57 1.49-.57 2.06 0l3.31 3.32c.57.57.57 1.49 0 2.06c-.28.29-.65.43-1.02.43Z"
+        fill="url(#f366id7)"
+      />
+      <path
+        d="M9.177 14.908a6.06 6.06 0 1 0 0-12.12a6.06 6.06 0 0 0 0 12.12Z"
+        fill="url(#f366id0)"
+      />
+      <path
+        d="M9.177 14.908a6.06 6.06 0 1 0 0-12.12a6.06 6.06 0 0 0 0 12.12Z"
+        fill="url(#f366id1)"
+      />
+      <g filter="url(#f366id9)">
+        <path
+          d="M9.125 14.531c0-3.347 2.216-5.031 5.563-5.031c0 3.684 1.372 6.061-5.563 5.031Z"
+          fill="#BC6965"
+        />
+      </g>
+      <path
+        d="M15.438 21.168a6.06 6.06 0 1 0 0-12.12a6.06 6.06 0 0 0 0 12.12Z"
+        fill="url(#f366id2)"
+      />
+      <path
+        d="M15.438 21.168a6.06 6.06 0 1 0 0-12.12a6.06 6.06 0 0 0 0 12.12Z"
+        fill="url(#f366id3)"
+      />
+      <g filter="url(#f366ida)">
+        <path
+          d="M14.798 20.69c.284-3.335 3.228-5.55 6.563-5.266c-.89 5.063-2.715 5.602-6.563 5.265Z"
+          fill="#BBB08B"
+        />
+      </g>
+      <path
+        d="M21.766 27.509a6.06 6.06 0 1 0 0-12.12a6.06 6.06 0 0 0 0 12.12Z"
+        fill="url(#f366id4)"
+      />
+      <path
+        d="M21.766 27.509a6.06 6.06 0 1 0 0-12.12a6.06 6.06 0 0 0 0 12.12Z"
+        fill="url(#f366id5)"
+      />
+      <path
+        d="M28.587 29.997c-.37 0-.74-.14-1.03-.43l-4.38-4.33c-.57-.57-.57-1.49 0-2.06c.57-.57 1.49-.57 2.06 0l4.38 4.33c.57.57.57 1.49 0 2.06c-.29.29-.66.43-1.03.43Z"
+        fill="url(#f366id8)"
+      />
+      <path
+        d="M28.587 29.997c-.37 0-.74-.14-1.03-.43l-4.38-4.33c-.57-.57-.57-1.49 0-2.06c.57-.57 1.49-.57 2.06 0l4.38 4.33c.57.57.57 1.49 0 2.06c-.29.29-.66.43-1.03.43Z"
+        fill="url(#f366id6)"
+      />
+      <defs>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="rotate(132.489 4.942 6.316) scale(10.086)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id0"
+          r={1}
+        >
+          <stop stopColor="#FFD3BC" />
+          <stop offset={0.422} stopColor="#E8A796" />
+          <stop offset={1} stopColor="#A8756C" />
+        </radialGradient>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="rotate(101.634 -2.682 11.65) scale(8.67828 12.0506)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id1"
+          r={1}
+        >
+          <stop stopColor="#BE7190" />
+          <stop offset={1} stopColor="#BF6D8E" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="rotate(119.97 5.972 12.244) scale(9.8841)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id2"
+          r={1}
+        >
+          <stop stopColor="#FFFED0" />
+          <stop offset={0.472} stopColor="#F6DDB8" />
+          <stop offset={1} stopColor="#B9A188" />
+        </radialGradient>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="matrix(-2.81247 7.68751 -9.21614 -3.37172 14.188 22.125)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id3"
+          r={1}
+        >
+          <stop stopColor="#D1A8CB" />
+          <stop offset={1} stopColor="#D0A5CB" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="rotate(113.674 6.229 18.034) scale(8.66685)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id4"
+          r={1}
+        >
+          <stop stopColor="#64DB91" />
+          <stop offset={0.471} stopColor="#37BE71" />
+          <stop offset={1} stopColor="#388E61" />
+        </radialGradient>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="rotate(95.029 -2.057 24.097) scale(8.78382)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id5"
+          r={1}
+        >
+          <stop stopColor="#5E8690" />
+          <stop offset={0.738} stopColor="#528787" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          cx={0}
+          cy={0}
+          gradientTransform="matrix(4.4375 4.81375 -3.2067 2.95606 27.188 26.374)"
+          gradientUnits="userSpaceOnUse"
+          id="f366id6"
+          r={1}
+        >
+          <stop offset={0.715} stopColor="#9B7549" stopOpacity={0} />
+          <stop offset={0.859} stopColor="#977246" />
+        </radialGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="f366id7"
+          x1={6.25}
+          x2={4.4}
+          y1={4.375}
+          y2={6.088}
+        >
+          <stop offset={0.162} stopColor="#FFD69D" />
+          <stop offset={0.558} stopColor="#E2A877" />
+          <stop offset={1} stopColor="#AB6D52" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="f366id8"
+          x1={27.625}
+          x2={25.563}
+          y1={25.5}
+          y2={27.5}
+        >
+          <stop offset={0.162} stopColor="#FFD9A3" />
+          <stop offset={0.439} stopColor="#F3B78D" />
+          <stop offset={0.887} stopColor="#BE706B" />
+        </linearGradient>
+        <filter
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+          height={7.273}
+          id="f366id9"
+          width={7.673}
+          x={8.125}
+          y={8.5}
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur
+            result="effect1_foregroundBlur_18_31282"
+            stdDeviation={0.5}
+          />
+        </filter>
+        <filter
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+          height={7.37}
+          id="f366ida"
+          width={8.563}
+          x={13.798}
+          y={14.399}
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur
+            result="effect1_foregroundBlur_18_31282"
+            stdDeviation={0.5}
+          />
+        </filter>
+      </defs>
+    </g>
+  </svg>
+}
+export default IconDango
