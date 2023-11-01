d6a222d612a2bb0eee2bb56816df465b3dff4061
TuanD <vincenttuandao@gmail.com>
2023-11-01 11:17:27 +0700

    feat: add more icons (#46)

---
+++ packages/ui/src/icons/components/icon-share.tsx
@@ -0,0 +1,22 @@
+import type { SVGProps } from 'react'
+
+function IconShare(props: SVGProps<SVGSVGElement>) {
+  return <svg
+    fill="none"
+    height="1em"
+    viewBox="0 0 16 17"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M9.8 5.70001V3.16667L14.3333 7.6L9.8 12.0333V9.5C9.8 9.5 3 8.91067 3 12.5C3 5.32 9.8 5.70001 9.8 5.70001Z"
+      fill="#343433"
+      stroke="#343433"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+    />
+  </svg>
+}
+export default IconShare
