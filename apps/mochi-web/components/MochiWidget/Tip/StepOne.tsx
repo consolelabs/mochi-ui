d43cb3f5b337d4ddb6b7cef3f3585c71047b3817
An Tran <tienan92it@gmail.com>
2023-11-02 10:36:58 +0700

    feat: add connect options button

--- apps/mochi-web/components/MochiWidget/Tip/StepOne.tsx
+++ apps/mochi-web/components/MochiWidget/Tip/StepOne.tsx
@@ -4,13 +4,43 @@ import { AmountInput } from '../AmountInput'
 import { Icon } from '@iconify/react'
 import { useTipWidget } from '.'
 import { useAuthStore } from '~store'
+import { useState } from 'react'
+
+interface ConnectButtonProps {
+  isOpenLogin: boolean
+}
+
+const ConnectButton: React.FC<ConnectButtonProps> = ({ isOpenLogin }) => {
+  const [isOpenLoginPopup, openLoginPopup] = useState(false)
+
+  function toggleLoginPopup() {
+    openLoginPopup(!isOpenLoginPopup)
+  }
+  return (
+    <button
+      onClick={toggleLoginPopup}
+      className="flex gap-x-1 justify-center items-center py-2.5 px-6 bg-blue-700 rounded-lg"
+    >
+      <span className="text-sm font-medium text-white-pure">
+        Connect options
+      </span>
+      <Icon
+        className={`w-5 h-5 text-white-pure transition ${
+          isOpenLoginPopup ? 'rotate-180' : ''
+        }`}
+        icon="majesticons:chevron-down-line"
+      />
+    </button>
+  )
+}
 
 export default function StepOne() {
   const { setStep } = useTipWidget()
-  const { token } = useAuthStore()
+  const { token, isLoggedIn } = useAuthStore()
+  const [isOpenLogin, openLogin] = useState(false)
 
   function openLoginPopup() {
-    // TODO: Implement login popup
+    openLogin(true)
   }
 
   return (
@@ -28,13 +58,17 @@ export default function StepOne() {
         <Recipient accessToken={token} onLoginRequest={openLoginPopup} />
         <AmountInput accessToken={token} onLoginRequest={openLoginPopup} />
       </div>
-      <button
-        onClick={() => setStep(2)}
-        className="flex gap-x-1 justify-center items-center py-2.5 px-6 bg-blue-700 rounded-lg"
-      >
-        <span className="text-sm font-medium text-white-pure">Continue</span>
-        <Icon className="w-5 h-5 text-white-pure" icon="ci:arrow-right-sm" />
-      </button>
+      {isLoggedIn ? (
+        <button
+          onClick={() => setStep(2)}
+          className="flex gap-x-1 justify-center items-center py-2.5 px-6 bg-blue-700 rounded-lg"
+        >
+          <span className="text-sm font-medium text-white-pure">Continue</span>
+          <Icon className="w-5 h-5 text-white-pure" icon="ci:arrow-right-sm" />
+        </button>
+      ) : (
+        <ConnectButton isOpenLogin={isOpenLogin} />
+      )}
     </div>
   )
 }
