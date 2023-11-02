fa4ac5ffa54076fb7329d6e08a047a1356bfcb53
tuanddd <vincenttuandao@gmail.com>
2023-11-02 15:11:32 +0700

    feat: add spinner

--- apps/mochi-web/components/AuthWidget/PanelContainer.tsx
+++ apps/mochi-web/components/AuthWidget/PanelContainer.tsx
@@ -12,7 +12,11 @@ export const PanelContainer: React.FC<PanelContainerProps> = ({
 }) => {
   switch (variant) {
     case 'dropdown':
-      return <div className="px-3 py-[22px] rounded-xl bg-white-pure">{children}</div>
+      return (
+        <div className="px-3 py-[22px] rounded-xl bg-white-pure">
+          {children}
+        </div>
+      )
     case 'modal':
     default:
       return <div className="p-6 rounded-xl bg-white-pure">{children}</div>
