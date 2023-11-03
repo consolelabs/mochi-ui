bcae157e6dea75e6e3e46e7fd84b36c926de06b2
tonible14012002 <namanhble14012002@gmail.com>
2023-11-03 14:31:23 +0700

    chore: update Icon name import

--- packages/ui/src/select/select.tsx
+++ packages/ui/src/select/select.tsx
@@ -1,95 +1,104 @@
-import type { ComponentPropsWithoutRef, ElementRef } from "react";
-import { forwardRef } from "react"
+import type { ComponentPropsWithoutRef, ElementRef } from 'react'
+import { forwardRef } from 'react'
 import * as SelectPrimitive from '@radix-ui/react-select'
-import clsx from "clsx";
-import { IconCheck, IconChevron } from "../icons";
-import { type SelectProps, type SelectItemProps, type SelectItemRef, type SelectTriggerRef, type SelectTriggerProps, type SelectGroupRef, SelectGroupProps} from "./type";
-import { getIconWrapperStyle, getSelectItemStyle } from "./utils";
+import clsx from 'clsx'
+import { IconCheck, IconChevron } from '../icons'
+import {
+  type SelectProps,
+  type SelectItemProps,
+  type SelectItemRef,
+  type SelectTriggerRef,
+  type SelectTriggerProps,
+  type SelectGroupRef,
+  SelectGroupProps,
+} from './type'
+import { getIconWrapperStyle, getSelectItemStyle } from './utils'
 
 function Select(props: SelectProps) {
   const { onChange } = props
   return (
     // Root has no ref
-    <SelectPrimitive.Root
-      onValueChange={onChange}
-      {...props}
-    />
+    <SelectPrimitive.Root onValueChange={onChange} {...props} />
   )
 }
 Select.displayName = SelectPrimitive.Root.displayName
 
-const SelectGroup = forwardRef<
-  SelectGroupRef,
-  SelectGroupProps
->(({className, ...props}, ref) => {
-  return (
-    <SelectPrimitive.Group
-      className={clsx(
-        "space-y-1",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+const SelectGroup = forwardRef<SelectGroupRef, SelectGroupProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <SelectPrimitive.Group
+        className={clsx('space-y-1', className)}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
 SelectGroup.displayName = SelectPrimitive.Group.displayName
 
 const SelectValue = SelectPrimitive.Value
 
-const SelectTrigger = forwardRef<
-  SelectTriggerRef,
-  SelectTriggerProps
->((props, ref) => {
-  const { children, disabled, asChild, className, hideRightIcon, rightIcon, leftIcon } = props
-  return (
-    <SelectPrimitive.Trigger
-      asChild={asChild}
-      className={getSelectItemStyle({
-        className,
-        isTrigger: true,
-        disabled
-      })}
-      disabled={disabled}
-      ref={ref}
-      {...props}
-    >
-      {asChild? children: (
-        <>
-      {leftIcon ? (
-        <SelectPrimitive.Icon asChild
-          className={getIconWrapperStyle()}
-        >
-          {leftIcon}
-        </SelectPrimitive.Icon>
-      ) : null}
-      {children}
-      {hideRightIcon ? null : (
-        <SelectPrimitive.Icon asChild
-          className={getIconWrapperStyle({isRightIcon: true})}
-        >
-          {rightIcon ?? <IconChevron className="text-sm"/>}
-        </SelectPrimitive.Icon>
-      )}
-        </>
-      )}
-    </SelectPrimitive.Trigger>
-  )}
+const SelectTrigger = forwardRef<SelectTriggerRef, SelectTriggerProps>(
+  (props, ref) => {
+    const {
+      children,
+      disabled,
+      asChild,
+      className,
+      hideRightIcon,
+      rightIcon,
+      leftIcon,
+    } = props
+    return (
+      <SelectPrimitive.Trigger
+        asChild={asChild}
+        className={getSelectItemStyle({
+          className,
+          isTrigger: true,
+          disabled,
+        })}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {asChild ? (
+          children
+        ) : (
+          <>
+            {leftIcon ? (
+              <SelectPrimitive.Icon asChild className={getIconWrapperStyle()}>
+                {leftIcon}
+              </SelectPrimitive.Icon>
+            ) : null}
+            {children}
+            {hideRightIcon ? null : (
+              <SelectPrimitive.Icon
+                asChild
+                className={getIconWrapperStyle({ isRightIcon: true })}
+              >
+                {rightIcon ?? <IconChevron className="text-sm" />}
+              </SelectPrimitive.Icon>
+            )}
+          </>
+        )}
+      </SelectPrimitive.Trigger>
+    )
+  },
 )
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
 
 const SelectContent = forwardRef<
   ElementRef<typeof SelectPrimitive.Content>,
   ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
+>(({ className, children, position = 'popper', ...props }, ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
       className={clsx(
-        "relative z-50 w-fit bg-white",
-        "rounded-lg shadow-md",
-        "space-y-1",
-        "p-3",
-        className
+        'relative z-50 w-fit bg-white',
+        'rounded-lg shadow-md',
+        'space-y-1',
+        'p-3',
+        className,
       )}
       position={position}
       ref={ref}
@@ -98,9 +107,9 @@ const SelectContent = forwardRef<
     >
       <SelectPrimitive.Viewport
         className={clsx(
-          "p-1",
-          position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+          'p-1',
+          position === 'popper' &&
+            'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]',
         )}
       >
         {children}
@@ -115,17 +124,17 @@ const SelectLabel = forwardRef<
   ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Label
-    className={clsx("text-[10px] uppercase font-bold text-neutral-500", className)}
+    className={clsx(
+      'text-[10px] uppercase font-bold text-neutral-500',
+      className,
+    )}
     ref={ref}
     {...props}
   />
 ))
 SelectLabel.displayName = SelectPrimitive.Label.displayName
 
-const SelectItem = forwardRef<
-  SelectItemRef,
-  SelectItemProps
->((props, ref) => {
+const SelectItem = forwardRef<SelectItemRef, SelectItemProps>((props, ref) => {
   const {
     children,
     className,
@@ -134,10 +143,10 @@ const SelectItem = forwardRef<
     rightIcon,
     extraRight,
     useIndicator,
-    subTitle
+    subTitle,
   } = props
 
-  const RightIconWrapper = useIndicator ? SelectPrimitive.ItemIndicator: 'span'
+  const RightIconWrapper = useIndicator ? SelectPrimitive.ItemIndicator : 'span'
 
   return (
     <SelectPrimitive.Item
@@ -148,24 +157,24 @@ const SelectItem = forwardRef<
       ref={ref}
       {...props}
     >
-      {leftIcon ? <span className={getIconWrapperStyle()}>
-          {leftIcon}
-        </span> : null}
+      {leftIcon ? (
+        <span className={getIconWrapperStyle()}>{leftIcon}</span>
+      ) : null}
       <div className="flex-1 flex flex-col items-start">
-        <SelectPrimitive.ItemText
-        >
-          {children}
-        </SelectPrimitive.ItemText>
-        {subTitle ? <span className="text-neutral-600 text-xs">
-            {subTitle}
-          </span> : null}
+        <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+        {subTitle ? (
+          <span className="text-neutral-600 text-xs">{subTitle}</span>
+        ) : null}
       </div>
       {extraRight}
-      {rightIcon || useIndicator && (
-        <RightIconWrapper className={getIconWrapperStyle({isRightIcon: true})}>
-          {rightIcon || <IconCheck/>}
-        </RightIconWrapper>
-      )}
+      {rightIcon ||
+        (useIndicator && (
+          <RightIconWrapper
+            className={getIconWrapperStyle({ isRightIcon: true })}
+          >
+            {rightIcon || <IconCheck />}
+          </RightIconWrapper>
+        ))}
     </SelectPrimitive.Item>
   )
 })
@@ -176,10 +185,7 @@ const SelectSeparator = forwardRef<
   ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Separator
-    className={clsx(
-      "my-3 h-px bg-neutral-200",
-      className
-    )}
+    className={clsx('my-3 h-px bg-neutral-200', className)}
     ref={ref}
     {...props}
   />
@@ -194,5 +200,5 @@ export {
   SelectContent,
   SelectLabel,
   SelectItem,
-  SelectSeparator ,
+  SelectSeparator,
 }
--- packages/ui/src/select/utils.ts
+++ packages/ui/src/select/utils.ts
@@ -1,55 +1,40 @@
-import { cva } from "class-variance-authority";
-
+import { cva } from 'class-variance-authority'
 
 export const getSelectItemStyle = cva(
-    [
-        "flex gap-2 items-center",
-        "transition duration-100",
-        "text-sm rounded-md"
-    ],{
-        variants: {
-            disabled: {
-                true: [
-                    "pointer-events-none",
-                    "text-neutral-600"
-                ],
-                false: ["text-neutral-800"]
-            },
-            isTrigger: {
-                false: [
-                    "font-medium ",
-                    "hover:bg-neutral-150",
-                    "hover:outline-none focus:outline-none",
-                    "cursor-pointer",
-                    "p-2 "
-                ],
-                true: [
-                    "px-2 py-1.5",
-                    "font-semibold",
-                    "shadow-sm"
-                ]
-            }
-        },
-        defaultVariants: {
-            disabled: false,
-            isTrigger: false
-        },
-        compoundVariants: [
-
-        ]
-    }
+  ['flex gap-2 items-center', 'transition duration-100', 'text-sm rounded-md'],
+  {
+    variants: {
+      disabled: {
+        true: ['pointer-events-none', 'text-neutral-600'],
+        false: ['text-neutral-800'],
+      },
+      isTrigger: {
+        false: [
+          'font-medium ',
+          'hover:bg-neutral-150',
+          'hover:outline-none focus:outline-none',
+          'cursor-pointer',
+          'p-2 ',
+        ],
+        true: ['px-2 py-1.5', 'font-semibold', 'shadow-sm'],
+      },
+    },
+    defaultVariants: {
+      disabled: false,
+      isTrigger: false,
+    },
+    compoundVariants: [],
+  },
 )
 
-export const getIconWrapperStyle = cva(
-    "", {
-        variants: {
-            isRightIcon: {
-                true: "text-base max-w-4 max-h-4 text-neutral-500",
-                false: "text-2xl max-w-6 max-h-6 text-inherit"
-            }
-        },
-        defaultVariants: {
-            isRightIcon: false
-        }
-    }
-)
+export const getIconWrapperStyle = cva('', {
+  variants: {
+    isRightIcon: {
+      true: 'text-base max-w-4 max-h-4 text-neutral-500',
+      false: 'text-2xl max-w-6 max-h-6 text-inherit',
+    },
+  },
+  defaultVariants: {
+    isRightIcon: false,
+  },
+})
