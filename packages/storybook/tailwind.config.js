408d6b5ffef96a64f240b9f50243d702817539e7
Khac Vy <khacvy93@gmail.com>
2023-11-07 14:48:53 +0700

    feat: cleanup

--- packages/storybook/tailwind.config.js
+++ packages/storybook/tailwind.config.js
@@ -2,8 +2,8 @@ import { mochi } from '@consolelabs/theme'
 
 module.exports = {
   content: [
-    '../components/*/src/**/*.{js,jsx,ts,tsx}',
-    '../components/*/src/**/*.stories.{js,jsx,ts,tsx}',
+    // '../components/*/src/**/*.{js,jsx,ts,tsx}',
+    // '../components/*/src/**/*.stories.{js,jsx,ts,tsx}',
     '../theme/src/components/**/*.{js,jsx,ts,tsx}',
   ],
   plugins: [mochi()],
