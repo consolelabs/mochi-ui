72fe3baf8e7c43873e0ab1f46b4e71eeb236ed55
Khac Vy <khacvy93@gmail.com>
2023-11-07 13:39:49 +0700

    fix: storybook build

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  // staticDirs: ['./assets'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-styling-webpack',
+  ],
+  webpackFinal: async (config) => {
+    // Resolve mjs files from libs
+    // Here we need the absolute paths to our other libs that the ui package depends on
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: [
+        /node_modules/,
+        // path.join(__dirname, '../../utils'),
+        // path.join(__dirname, '../../hooks'),
+      ],
+      type: 'javascript/auto',
+    })
+
+    return config
+  },
+}
---
+++ packages/ui/.storybook/preview.js
@@ -0,0 +1,15 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context' // next 11.2
+import '../src/styles.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
