535bcdbdc0e0d947a850001c26ad0c5ff2421b79
tonible14012002 <namanhble14012002@gmail.com>
2023-11-09 12:41:01 +0700

    feat: create separator ui component

---
+++ packages/theme/src/components/separator.ts
@@ -0,0 +1,11 @@
+const horizontal = 'h-[0.5px] w-full'
+const vertical = 'w-[0.5px] h-full'
+const base = 'shrink-0 bg-neutral-400 rounded-full'
+
+const separator = {
+  horizontal,
+  vertical,
+  base,
+}
+
+export { separator }
