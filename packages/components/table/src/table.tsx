bf84721a27645552339aad1db19c697fb432b511
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-10 16:52:20 +0700

    refactor: convert pagination and table to component packages

---
+++ packages/components/table/src/table.tsx
@@ -0,0 +1,116 @@
+import type { RowData, ColumnDef as ColDef } from '@tanstack/react-table'
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import type { ReactNode } from 'react'
+import { table } from '@consolelabs/theme'
+
+export type ColumnProps<T> = ColDef<T>
+
+export interface TableProps<T> {
+  columns: ColumnProps<T>[]
+  data: T[]
+  isLoading?: boolean
+  loadingRows?: number
+}
+
+const {
+  tableWrapperClsx,
+  tableClsx,
+  tableHeaderClsx,
+  tableDataLoadingClsx,
+  tableDataSkeletonClsx,
+  tableDataClsx,
+} = table
+
+export default function Table<T extends RowData>({
+  data,
+  columns,
+  isLoading,
+  loadingRows = 5,
+}: TableProps<T>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  })
+
+  return (
+    <div className={tableWrapperClsx()}>
+      <table className={tableClsx()}>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  className={tableHeaderClsx()}
+                  colSpan={header.colSpan}
+                  key={header.id}
+                >
+                  {!header.isPlaceholder &&
+                    (flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    ) as ReactNode)}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {isLoading
+            ? new Array(loadingRows)
+                .fill(
+                  new Array(table.getHeaderGroups()[0].headers.length).fill(0),
+                )
+                .map((headers, rowIdx) => (
+                  <tr key={rowIdx}>
+                    {headers.map((_: any, idx: number) => (
+                      <td className={tableDataLoadingClsx()} key={idx}>
+                        <div className={tableDataSkeletonClsx()} />
+                      </td>
+                    ))}
+                  </tr>
+                ))
+            : null}
+
+          {!isLoading
+            ? table.getRowModel().rows.map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td className={tableDataClsx()} key={cell.id}>
+                      {
+                        flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        ) as ReactNode
+                      }
+                    </td>
+                  ))}
+                </tr>
+              ))
+            : null}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th colSpan={header.colSpan} key={header.id}>
+                  {!header.isPlaceholder &&
+                    (flexRender(
+                      header.column.columnDef.footer,
+                      header.getContext(),
+                    ) as ReactNode)}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  )
+}
---
+++ packages/components/table/stories/table.stories.tsx
@@ -0,0 +1,136 @@
+/* eslint-disable react-hooks/rules-of-hooks -- . */
+import type { Meta, StoryObj } from '@storybook/react'
+import { useCallback, useMemo, useRef, useState } from 'react'
+import { IconStar } from '@consolelabs/icons'
+import { Badge } from '@consolelabs/badge'
+import { Pagination } from '@consolelabs/pagination'
+import Table from '../src/table'
+
+const meta: Meta<typeof Table> = {
+  title: 'ui/Table',
+  component: Table,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  argTypes: {
+    isLoading: {
+      type: 'boolean',
+    },
+    data: {
+      type: {
+        name: 'array',
+        value: {
+          name: 'other',
+          value: 'any',
+        },
+        required: true,
+      },
+    },
+    columns: {
+      type: {
+        name: 'array',
+        value: {
+          name: 'other',
+          value: 'any',
+        },
+        required: true,
+      },
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Table>
+
+const data = new Array(50)
+  .fill({
+    id: 0,
+    name: 'John Doe',
+    email: 'hello world',
+    role: 'get-role',
+  })
+  .map((item, index) => ({
+    ...item,
+    id: index + 1,
+    name: `John Doe #${index + 1}`,
+  }))
+
+export const Default: Story = {
+  render: () => {
+    const [itemPerPage, setItemPerPage] = useState(5)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+    const timeoutRef = useRef<number | NodeJS.Timeout>()
+
+    const dataList = useMemo(() => {
+      const chunkedArray = []
+      for (let i = 0; i < data.length; i += itemPerPage) {
+        chunkedArray.push(data.slice(i, i + itemPerPage))
+      }
+      return chunkedArray
+    }, [itemPerPage])
+
+    const onPageChange = useCallback((pg: number) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+      setLoading(true)
+      timeoutRef.current = setTimeout(() => {
+        setPage(pg)
+        setLoading(false)
+      }, 500)
+    }, [])
+
+    return (
+      <div className="p-4 min-w-[48rem]">
+        <Table<(typeof data)[0]>
+          columns={[
+            {
+              header: 'ID',
+              accessorKey: 'id',
+              cell(props) {
+                return (
+                  <span className="w-32">{props.getValue() as string}</span>
+                )
+              },
+            },
+            {
+              header: 'Name',
+              accessorKey: 'name',
+            },
+            {
+              header: 'Email',
+              accessorKey: 'email',
+            },
+            {
+              header: 'Role',
+              accessorKey: 'role',
+              cell: (cell) => {
+                return (
+                  <Badge
+                    appearance="primary"
+                    icon={<IconStar />}
+                    label={cell.getValue() as string}
+                  />
+                )
+              },
+            },
+          ]}
+          data={dataList[page - 1]}
+          isLoading={loading}
+        />
+        <Pagination
+          initItemsPerPage={itemPerPage}
+          initalPage={page}
+          onItemPerPageChange={setItemPerPage}
+          onPageChange={onPageChange}
+          totalItems={data.length}
+          totalPages={Math.ceil(data.length / itemPerPage)}
+        />
+      </div>
+    )
+  },
+}
