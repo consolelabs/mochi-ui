9b5f9a11a3aa6866c34df96c3a67ad465dee2915
Chinh Le <71743905+chinhld12@users.noreply.github.com>
2023-11-10 15:04:47 +0700

    feat: test gh action (#124)

---
+++ packages/components/button/__tests__/button.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, render } from '@testing-library/react'
+import Button from '../src/button'
+
+describe('Button', () => {
+  it('renders the button with the correct text', () => {
+    const buttonText = 'Click me'
+    const { getByText } = render(<Button>{buttonText}</Button>)
+    const button = getByText(buttonText)
+    expect(button).toBeInTheDocument()
+  })
+
+  it('calls the onClick function when clicked', () => {
+    const onClick = jest.fn()
+    const { getByText } = render(<Button onClick={onClick}>Click me</Button>)
+    const button = getByText('Click me')
+    fireEvent.click(button)
+    expect(onClick).toHaveBeenCalled()
+  })
+
+  it('disables the button when loading', () => {
+    const { container } = render(<Button loading>Click me</Button>)
+    const button = container.querySelector('button')
+    expect(button).toHaveClass('pointer-events-none')
+  })
+})
