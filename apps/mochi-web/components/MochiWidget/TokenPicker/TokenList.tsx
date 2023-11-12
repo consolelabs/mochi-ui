b88f2761d107a8c50896442bd091cef25665641f
An Tran <tienan92it@gmail.com>
2023-11-13 02:24:09 +0700

    chore: sort TokenList by balance amount

--- apps/mochi-web/components/MochiWidget/TokenPicker/TokenList.tsx
+++ apps/mochi-web/components/MochiWidget/TokenPicker/TokenList.tsx
@@ -1,18 +1,21 @@
 import { List } from '@consolelabs/ui-components'
-import { ModelBalance } from '~types/mochi-pay-schema'
+import { Balance } from '~store'
 import { TokenItem } from './TokenItem'
 import { EmptyList } from './EmptyList'
 
 interface Props {
-  data: ModelBalance[]
-  onSelect?: (item: ModelBalance) => void
+  data: Balance[]
+  onSelect?: (item: Balance) => void
 }
 
 export const TokenList = (props: Props) => {
   const { data, onSelect } = props
+  const sortedData = data.sort((a, b) => {
+    return (b.usd_balance ?? 0) - (a.usd_balance ?? 0)
+  })
   return (
     <List
-      data={data}
+      data={sortedData}
       renderItem={(item) => <TokenItem item={item} onSelect={onSelect} />}
       ListEmpty={<EmptyList />}
       rootClassName="w-full h-full"
