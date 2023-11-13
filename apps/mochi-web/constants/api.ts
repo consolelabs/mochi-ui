d6b6cf9ccc2c785e8d0e39699d1e90c2efe5bfa3
tuanddd <vincenttuandao@gmail.com>
2023-11-13 14:53:12 +0700

    fix: add timeout in tx page

--- apps/mochi-web/constants/api.ts
+++ apps/mochi-web/constants/api.ts
@@ -1,17 +1,23 @@
 import wretch from 'wretch'
 import QueryStringAddon from 'wretch/addons/queryString'
+import AbortAddon from 'wretch/addons/abort'
 
 import { MOCHI_PAY_API, MOCHI_PROFILE_API, MOCHI_API } from '../envs'
 
 const MOCHI_PROFILE = wretch(MOCHI_PROFILE_API)
   .addon(QueryStringAddon)
+  .addon(AbortAddon())
   .errorType('json')
 
 const MOCHI_PAY = wretch(MOCHI_PAY_API)
   .addon(QueryStringAddon)
+  .addon(AbortAddon())
   .errorType('json')
 
-const MOCHI = wretch(MOCHI_API).addon(QueryStringAddon).errorType('json')
+const MOCHI = wretch(MOCHI_API)
+  .addon(QueryStringAddon)
+  .addon(AbortAddon())
+  .errorType('json')
 
 export const API = {
   MOCHI_PROFILE,
