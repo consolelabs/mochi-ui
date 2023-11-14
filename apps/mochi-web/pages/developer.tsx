0233ad802fa3d950831b3734e0becbe6c72184ce
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-14 15:59:14 +0700

    refactor: migrate tooltip component

--- apps/mochi-web/pages/developer.tsx
+++ apps/mochi-web/pages/developer.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image'
 import clsx from 'clsx'
 import { Layout } from '~app/layout'
 import { SEO } from '~app/layout/seo'
-import { Tooltip } from '@consolelabs/ui-components'
-import { Button, IconButton } from '@consolelabs/core'
+import { Button, IconButton, Tooltip } from '@consolelabs/core'
 import {
   IconApt,
   IconArb,
