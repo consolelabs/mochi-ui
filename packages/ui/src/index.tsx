ba62ca2580bb568561dff444f4725c1ee2f15785
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-11-15 14:37:44 +0700

    chore: remove switch & dropdown component from ui package (#202)

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,14 +1,12 @@
 // components
 export * from './accordion'
 export * from './alert'
-export * from './dropdown'
 export * from './heading'
 export * from './input-field'
 export * from './popover'
 export * from './util'
 export * from './login-widget'
 export * from './popover'
-export * from './switch'
 export * from './input'
 
 export * from './util'
