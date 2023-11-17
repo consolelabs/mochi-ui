663e2c65162a2fdad94faf16dcebc527b8f86a73
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-17 18:05:44 +0700

    feat: update input unit test

--- packages/components/input/__tests__/input.test.tsx
+++ packages/components/input/__tests__/input.test.tsx
@@ -3,37 +3,37 @@ import Input from '../src/input'
 
 describe('Input', () => {
   it('renders an input element', () => {
-    const { container } = render(<Input />)
+    const { container } = render(<Input.InputField />)
     const input = container.querySelector('input')
     expect(input).toBeInTheDocument()
   })
 
   it('applies the correct className', () => {
-    const { container } = render(<Input className="test-class" />)
+    const { container } = render(<Input.InputField className="test-class" />)
     const input = container.querySelector('input')
     expect(input).toHaveClass('test-class')
   })
 
   it('applies the correct size variant', () => {
-    const { container } = render(<Input size="large" />)
+    const { container } = render(<Input.InputField size="lg" />)
     const input = container.querySelector('input')
-    expect(input).toHaveClass('text-md leading-snug')
+    expect(input).toHaveClass('h-[52px] text-md leading-snug')
   })
 
   it('applies the disabled attribute', () => {
-    const { container } = render(<Input disabled />)
+    const { container } = render(<Input.InputField disabled />)
     const input = container.querySelector('input')
     expect(input).toBeDisabled()
   })
 
   it('applies the error variant', () => {
-    const { container } = render(<Input error />)
+    const { container } = render(<Input.InputField error />)
     const input = container.querySelector('input')
     expect(input).toHaveClass('border-red-700 focus:border-red-700')
   })
 
   it('passes through any other props', () => {
-    const { container } = render(<Input data-testid="test-input" />)
+    const { container } = render(<Input.InputField data-testid="test-input" />)
     const input = container.querySelector('input')
     expect(input).toHaveAttribute('data-testid', 'test-input')
   })
