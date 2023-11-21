8b8042327aa304d9316d1948f377dc85e654f1e7
Khac Vy <khacvy93@gmail.com>
2023-11-21 14:55:49 +0700

    Chore/update separator color (#254)
    * chore: update separator semantic color

--- packages/theme/src/components/separator.ts
+++ packages/theme/src/components/separator.ts
@@ -1,6 +1,6 @@
 const horizontal = 'h-[0.5px] w-full'
 const vertical = 'w-[0.5px] h-full'
-const base = 'shrink-0 bg-neutral-400 rounded-full'
+const base = 'shrink-0 bg-divider rounded-full'
 
 export const separator = {
   horizontal,
