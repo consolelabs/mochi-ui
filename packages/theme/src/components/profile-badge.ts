9a02787fc9665dbfed91d848d3ca46d11695f795
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-21 14:17:02 +0700

    chore: fix wrong tailwind class usage

--- packages/theme/src/components/profile-badge.ts
+++ packages/theme/src/components/profile-badge.ts
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 
 const profileBadgeClsx = ({ className = '' }: { className?: string } = {}) =>
   clsx(
-    'flex gap-x-2 items-center p-1 pr-2 rounded-lg border transition border-neutral-outline-border bg-surface hover:bg-neutral-outline',
+    'flex gap-x-2 items-center p-1 pr-2 rounded-lg border transition border-neutral-outline-border bg-background-surface hover:bg-neutral-outline',
     className,
   )
 
