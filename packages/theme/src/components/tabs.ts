a3cca2769f44aa718b702b20ec177e3ac6591eff
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-21 17:26:22 +0700

    refactor: use semantic tailwind classes in tabs

--- packages/theme/src/components/tabs.ts
+++ packages/theme/src/components/tabs.ts
@@ -19,19 +19,19 @@ const tabTriggerVariants = cva(
     compoundVariants: [
       {
         disabled: true,
-        className: 'text-neutral-400 cursor-not-allowed',
+        className: 'text-text-tertiary cursor-not-allowed',
       },
       {
         disabled: false,
         variant: 'solid',
         className:
-          'rounded text-neutral-600 hover:text-neutral-500 hover:bg-neutral-150 data-[state=active]:text-neutral-800 data-[state=active]:hover:text-neutral-500',
+          'rounded text-neutral-solid hover:text-text-secondary hover:bg-neutral-outline-hover data-[state=active]:text-neutral-solid-active data-[state=active]:hover:text-text-secondary',
       },
       {
         disabled: false,
         variant: 'link',
         className:
-          'text-neutral-600 hover:text-neutral-700 data-[state=active]:text-neutral-800 data-[state=active]:hover:text-neutral-700',
+          'text-text-secondary hover:text-neutral-plain-fg data-[state=active]:text-text-primary data-[state=active]:hover:text-neutral-700',
       },
     ],
     defaultVariants: {
@@ -43,7 +43,7 @@ const tabTriggerVariants = cva(
 
 const tabTriggerWrapperClassName = ({ className }: { className?: string }) =>
   clsx(
-    'inline-flex flex-1 px-2 border-r border-r-neutral-200 last:border-none sm:border-none',
+    'inline-flex flex-1 px-2 border-r border-r-neutral-outline-active last:border-none sm:border-none',
     className,
   )
 
