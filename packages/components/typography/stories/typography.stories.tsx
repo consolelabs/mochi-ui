ca8cf7467c40c98e64fd408d4472614e3d188748
Khac Vy <khacvy93@gmail.com>
2023-11-21 14:40:41 +0700

    feat: update semantic color for Typo

--- packages/components/typography/stories/typography.stories.tsx
+++ packages/components/typography/stories/typography.stories.tsx
@@ -1,14 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import Typography from '../src/typography'
+import Typography, { TypographyProps } from '../src/typography'
 
 const meta: Meta<typeof Typography> = {
   title: 'components/Typography',
   component: Typography,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {
     level: {
@@ -32,16 +30,15 @@ const meta: Meta<typeof Typography> = {
       control: 'select',
       options: [
         'primary',
+        'secondary',
         'success',
         'warning',
         'danger',
         'neutral',
-        undefined,
+        'textPrimary',
+        'textSecondary',
+        'textTertiary',
       ],
-    },
-    variant: {
-      control: 'select',
-      options: ['plain', 'outlined', 'soft', 'solid', undefined],
     },
     noWrap: {
       control: 'boolean',
@@ -54,7 +51,58 @@ type Story = StoryObj<typeof Typography>
 
 export const Default: Story = {
   args: {
-    children:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    children: 'Lorem ipsum',
   },
+}
+
+const levelVariants: TypographyProps['level'][] = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'title-lg',
+  'title-md',
+  'title-sm',
+  'body-lg',
+  'body-md',
+  'body-sm',
+  'body-xs',
+  'inherit',
+]
+
+export const Levels: Story = {
+  render: () => (
+    <div className="flex flex-col gap-2">
+      {levelVariants.map((level) => (
+        <Typography key={level} level={level}>
+          Lorem ipsum
+        </Typography>
+      ))}
+    </div>
+  ),
+}
+
+const colorVariants: TypographyProps['color'][] = [
+  'primary',
+  'secondary',
+  'danger',
+  'success',
+
+  'warning',
+  'neutral',
+  'textPrimary',
+  'textSecondary',
+  'textTertiary',
+]
+
+export const Colors: Story = {
+  render: () => (
+    <div className="flex flex-col gap-2">
+      {colorVariants.map((color) => (
+        <Typography key={color} color={color}>
+          Lorem ipsum
+        </Typography>
+      ))}
+    </div>
+  ),
 }
--- packages/theme/src/components/typography.ts
+++ packages/theme/src/components/typography.ts
@@ -3,10 +3,12 @@ import { cva, VariantProps } from 'class-variance-authority'
 const typographyVariants = cva(['overflow-hidden'], {
   variants: {
     level: {
-      h1: 'text-4xl font-bold',
-      h2: 'text-3xl font-bold',
-      h3: 'text-2xl font-semibold',
-      h4: 'text-xl font-semibold',
+      h1: 'text-7xl font-bold',
+      h2: 'text-5xl font-bold',
+      h3: 'text-[40px] font-semibold',
+      h4: 'text-[32px] font-semibold',
+      h5: 'text-2xl font-semibold',
+      h6: 'text-lg font-semibold',
       'title-lg': 'text-lg font-semibold',
       'title-md': 'text-base font-medium',
       'title-sm': 'text-sm font-medium',
@@ -17,62 +19,24 @@ const typographyVariants = cva(['overflow-hidden'], {
       inherit: 'text-inherit',
     },
     color: {
-      primary: 'text-primary-700 border-primary-700',
-      success: 'text-green-700 border-green-700',
-      warning: 'text-yellow-700 border-yellow-700',
-      danger: 'text-red-700 border-red-700',
-      neutral: 'text-neutral-800 border-neutral-800',
-    },
-    variant: {
-      plain: '-mx-2 px-2 text-opacity-80',
-      outlined: '-mx-2 px-2 border text-opacity-70',
-      soft: '-mx-2 px-2 bg-opacity-20',
-      solid: '-mx-2 px-2',
+      primary: 'text-primary-plain-fg',
+      secondary: 'text-secondary-plain-fg',
+      success: 'text-success-plain-fg',
+      warning: 'text-warning-plain-fg',
+      danger: 'text-danger-plain-fg',
+      neutral: 'text-neutral-plain-fg',
+      textPrimary: 'text-text-primary',
+      textSecondary: 'text-text-secondary',
+      textTertiary: 'text-text-tertiary',
     },
     noWrap: {
       true: 'text-ellipsis whitespace-nowrap',
     },
   },
-  compoundVariants: [
-    {
-      color: null,
-      className: 'border-0',
-    },
-    {
-      color: ['primary', 'success', 'warning', 'danger', 'neutral'],
-      variant: 'solid',
-      className: '!text-white',
-    },
-    {
-      color: 'primary',
-      variant: ['soft', 'solid'],
-      className: 'bg-primary-600',
-    },
-    {
-      color: 'success',
-      variant: ['soft', 'solid'],
-      className: 'bg-green-600',
-    },
-    {
-      color: 'warning',
-      variant: ['soft', 'solid'],
-      className: 'bg-yellow-600',
-    },
-    {
-      color: 'danger',
-      variant: ['soft', 'solid'],
-      className: 'bg-red-600',
-    },
-    {
-      color: 'neutral',
-      variant: ['soft', 'solid'],
-      className: 'bg-neutral-600',
-    },
-  ],
+
   defaultVariants: {
     level: 'body-md',
-    variant: null,
-    color: null,
+    color: 'textPrimary',
     noWrap: false,
   },
 })
@@ -81,6 +45,6 @@ const typography = {
   typographyVariants,
 }
 
-export type TypographyProps = VariantProps<typeof typographyVariants>
+export type TypographyStyleProps = VariantProps<typeof typographyVariants>
 
 export { typography }
