a50496823bdfb169b5a8c30c1849cdb07534de78
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-11-23 09:55:07 +0700

    feat: create page header component (#278)

---
+++ packages/components/page-header/README.md
@@ -0,0 +1,30 @@
+# @consolelabs/page-header
+
+Page Header component is designed to encapsulate essential details about the
+current page like page title and description. You can also integrate interactive
+elements, such as buttons and selects, directly into the header.
+
+## Installation
+
+Feel free to use the package manager of your choice:
+
+```sh
+# Using PNPM
+pnpm i @consolelabs/page-header
+
+# Using NPM
+npm i @consolelabs/page-header
+
+# Using Yarn
+yarn add @consolelabs/page-header
+```
+
+## Contribution
+
+We welcome your contributions! For guidance on how to contribute, please refer
+to our [Contribution Guidelines](/CONTRIBUTING.md).
+
+## Licence
+
+This project is licensed under the terms of the
+[MIT license](https://choosealicense.com/licenses/mit/).
---
+++ packages/components/page-header/__tests__/page-header.test.tsx
@@ -0,0 +1,75 @@
+import userEvent from '@testing-library/user-event'
+import { render, waitFor } from '@testing-library/react'
+import { Button } from '@consolelabs/button'
+import { PageHeader } from '../src'
+
+describe('Page Header', () => {
+  it('renders the title correctly', () => {
+    const { container } = render(
+      <PageHeader title="Page Title" titleExtra="(2,951 members)" />,
+    )
+
+    const title = container.querySelector('h5')
+    expect(title?.innerHTML || '').toBe('Page Title')
+  })
+
+  it('renders the title extra and description correctly', () => {
+    const descriptionValue =
+      'Lorem ipsum dolor sit amet consectetur. Sed turpis eget sed nullam volutpat integer posuere.'
+    const titleExtraValue = '(2,951 members)'
+
+    const { container } = render(
+      <PageHeader
+        title="Page Title"
+        titleExtra={titleExtraValue}
+        description={descriptionValue}
+      />,
+    )
+
+    const description = container.querySelector('p')
+    const titleExtra = container.querySelector('span')
+
+    expect(description?.innerHTML || '').toBe(descriptionValue)
+    expect(titleExtra?.innerHTML || '').toBe(titleExtraValue)
+  })
+
+  it('applies the backHref correctly', () => {
+    const hrefValue = '/test'
+
+    const { container } = render(
+      <PageHeader backHref={hrefValue} title="Page Title" />,
+    )
+
+    const title = container.querySelector('a')
+    expect(title?.getAttribute('href')).toBe(hrefValue)
+  })
+
+  it('applies the onBack function correctly', async () => {
+    const mockFunction = jest.fn()
+
+    const { container } = render(
+      <PageHeader onBack={mockFunction} title="Page Title" />,
+    )
+
+    const backButton = container.querySelector('button')
+    if (backButton) {
+      userEvent.click(backButton)
+    }
+
+    await waitFor(() => {
+      expect(mockFunction).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it('renders the actions correctly', () => {
+    const { container } = render(
+      <PageHeader
+        title="Page Title"
+        actions={[<Button>Button one</Button>, <Button>Button two</Button>]}
+      />,
+    )
+
+    const buttons = container.querySelectorAll('button')
+    expect(Array.from(buttons).length).toBe(2)
+  })
+})
