8e46b32aa9f4602808af94c108c00127665b6b43
Khac Vy <khacvy93@gmail.com>
2023-11-26 11:29:44 +0700

    fix: update tailwind class

--- packages/theme/src/components/label.ts
+++ packages/theme/src/components/label.ts
@@ -2,7 +2,7 @@ import { clsx } from 'clsx'
 
 const labelClsx = ({ className = '' }: { className?: string }) =>
   clsx(
-    'tracking-tighter uppercase text-text-secondary text-[10px] font-bold',
+    'tracking-tighter uppercase text-text-secondary text-xxs font-bold',
     className,
   )
 
