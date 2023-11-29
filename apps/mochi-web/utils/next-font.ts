f2850183905dc24c7ae1979b3c2508c13df8c6de
Chinh Le <71743905+chinhld12@users.noreply.github.com>
2023-11-29 12:46:41 +0700

    chore: update using local fonts (#324)

---
+++ apps/mochi-web/utils/next-font.ts
@@ -0,0 +1,55 @@
+import localFont from 'next/font/local'
+
+export const interFont = localFont({
+  src: [
+    {
+      path: '../assets/Inter-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../assets/Inter-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-inter',
+  preload: true,
+  fallback: ['Roboto', 'sans-serif', 'system-ui', 'arial'],
+  display: 'swap',
+})
