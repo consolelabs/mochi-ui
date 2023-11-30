d5c0aad38caa70f5b8162bfa721db871e62ba75b
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-30 14:09:25 +0700

    chore: add divider to mochi web header

--- apps/mochi-web/components/Header/Header.tsx
+++ apps/mochi-web/components/Header/Header.tsx
@@ -438,6 +438,9 @@ export const Header = () => {
           >
             Roadmap
           </Link>,
+          <div className="w-px h-full -mx-2" key="desktop-nav-divider">
+            <div className="w-auto h-6 my-auto bg-[#eeedec]" />
+          </div>,
           <LoginPopover isLogging={isLogging} key="desktop-login-popover" />,
         ]),
   ]
