54c8dfd15aca0e11e1dfc58f622de779e1d6e74e
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-11-30 13:33:38 +0700

    chore: update use lock screen hook

--- packages/components/top-bar/hooks/useLockScreenScroll.ts
+++ packages/components/top-bar/hooks/useLockScreenScroll.ts
@@ -4,13 +4,20 @@ export function useLockScreenScroll(): void {
   // useLaoutEffect callback return type is "() => void" type
   useLayoutEffect((): (() => void) => {
     // Get original body overflow
-    const wrapper = document.getElementsByTagName('body')?.[0] as HTMLElement
-    const originalStyle: string = window?.getComputedStyle(wrapper)?.overflow
-    // Prevent scrolling on mount
-    wrapper.style.overflow = 'hidden'
-    // Re-enable scrolling when component unmounts
+    const wrapper = document?.getElementById('__next') as HTMLElement
+    let originalStyle = ''
+
+    if (wrapper) {
+      originalStyle = window?.getComputedStyle(wrapper)?.overflow || 'visible'
+      // Prevent scrolling on mount
+      wrapper.style.overflow = 'hidden'
+      // Re-enable scrolling when component unmounts
+    }
+
     return () => {
-      wrapper.style.overflow = originalStyle
+      if (wrapper) {
+        wrapper.style.overflow = originalStyle
+      }
     }
   }, []) // Empty array ensures effect is only run on mount and unmount
 }
