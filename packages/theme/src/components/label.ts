8a03c7cabe39868c42e7beca729209c1f209aea7
Thanh Pham <zlatan.pham@gmail.com>
2023-12-01 14:02:20 +0700

    fix(label): loose label tracking letter (#340)

--- packages/theme/src/components/label.ts
+++ packages/theme/src/components/label.ts
@@ -2,7 +2,7 @@ import { clsx } from 'clsx'
 
 const labelClsx = ({ className = '' }: { className?: string }) =>
   clsx(
-    'tracking-tighter uppercase text-text-secondary text-xxs font-bold',
+    'tracking-tight uppercase text-text-secondary text-xxs font-bold',
     className,
   )
 
