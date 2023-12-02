f0a0d878c7a0a32b0131b8597926d40b7db18931
Chinh Le <71743905+chinhld12@users.noreply.github.com>
2023-12-02 09:39:31 +0700

    chore: add turbo cache for local using in github actions (#342)
    * chore: add turbo cache for local using in github actions
    
    * chore: update test config

--- packages/storybook/tailwind.config.js
+++ packages/storybook/tailwind.config.js
@@ -2,7 +2,6 @@ import { consolelabs } from '@consolelabs/theme'
 
 module.exports = {
   content: [
-    // '../components/*/src/**/*.{js,jsx,ts,tsx}',
     '../components/*/stories/**/*.stories.{js,jsx,ts,tsx}',
     '../theme/src/components/**/*.{js,jsx,ts,tsx}',
   ],
