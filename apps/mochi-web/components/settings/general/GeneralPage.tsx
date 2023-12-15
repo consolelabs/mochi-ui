bc7174cc97b99f44d543a03424bb2674e75c65d5
Thanh Pham <zlatan.pham@gmail.com>
2023-12-15 14:35:34 +0700

    chore: add logout button

--- apps/mochi-web/components/settings/general/GeneralPage.tsx
+++ apps/mochi-web/components/settings/general/GeneralPage.tsx
@@ -1,5 +1,24 @@
+import { Button, useLoginWidget } from '@mochi-ui/core'
+import { useRouter } from 'next/router'
 import React from 'react'
+import { ROUTES } from '~constants/routes'
 
 export const GeneralPage = () => {
-  return <div>General</div>
+  const { logout } = useLoginWidget()
+  const { replace } = useRouter()
+
+  return (
+    <Button
+      variant="outline"
+      color="white"
+      onClick={() => {
+        replace(ROUTES.HOME)
+        setTimeout(() => {
+          logout()
+        }, 500)
+      }}
+    >
+      Log out
+    </Button>
+  )
 }
