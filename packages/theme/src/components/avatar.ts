97ba0241b19a22f6f403baca253ea97b94bb349c
Thanh Pham <zlatan.pham@gmail.com>
2023-12-19 11:16:47 +0700

    fix(avatar): hidden small avatar (#452)

--- packages/theme/src/components/avatar.ts
+++ packages/theme/src/components/avatar.ts
@@ -1,22 +1,19 @@
 import { cva, VariantProps } from 'class-variance-authority'
 
-const avatarCva = cva(
-  ['inline-block flex-shrink-0 rounded-full overflow-hidden'],
-  {
-    variants: {
-      size: {
-        xs: 'w-4 h-4',
-        sm: 'w-7 h-7',
-        base: 'w-10 h-10',
-        lg: 'w-14 h-14',
-        xl: 'w-20 h-20',
-      },
-    },
-    defaultVariants: {
-      size: 'base',
+const avatarCva = cva(['inline-block flex-shrink-0 rounded-full'], {
+  variants: {
+    size: {
+      xs: 'w-4 h-4',
+      sm: 'w-7 h-7',
+      base: 'w-10 h-10',
+      lg: 'w-14 h-14',
+      xl: 'w-20 h-20',
     },
   },
-)
+  defaultVariants: {
+    size: 'base',
+  },
+})
 
 const avatarImgClsx = 'h-full'
 
