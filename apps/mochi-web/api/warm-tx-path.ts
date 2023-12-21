0efc1834fb478ff4f43b61f0374f7a45299ea054
Thanh Pham <zlatan.pham@gmail.com>
2023-12-21 15:47:20 +0700

    perf(mochi-web): improve tx load time

--- apps/mochi-web/api/warm-tx-path.ts
+++ apps/mochi-web/api/warm-tx-path.ts
@@ -8,13 +8,13 @@ export default async function handler(
     const response = await fetch(`${process.env.VERCEL_URL}/tx/e39b334f7d0c`)
     if (response.ok) {
       // Request was successful
-      res.status(200).json({ success: true })
+      res.status(200).json({ success: true, url: process.env.VERCEL_URL })
     } else {
       // Request failed
-      res.status(500).json({ success: false })
+      res.status(500).json({ success: false, url: process.env.VERCEL_URL })
     }
   } catch (error) {
     // Error occurred during the request
-    res.status(500).json({ success: false })
+    res.status(500).json({ success: false, url: process.env.VERCEL_URL })
   }
 }
