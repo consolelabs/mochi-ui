83e609dee99227fbe8ef12ecf3c39e7434c41cba
Thanh Pham <zlatan.pham@gmail.com>
2023-12-21 14:18:53 +0700

    perf(mochi-web): improve tx load time

---
+++ apps/mochi-web/api/warm-tx-path.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const response = await fetch('https://beta.mochi.gg/tx/e39b334f7d0c')
+    if (response.ok) {
+      // Request was successful
+      res.status(200).json({ success: true })
+    } else {
+      // Request failed
+      res.status(500).json({ success: false })
+    }
+  } catch (error) {
+    // Error occurred during the request
+    res.status(500).json({ success: false })
+  }
+}
