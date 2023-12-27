071b354ef9f4c4d355846de049e5ffae4dc3d3c6
Hien Le <leduyhien152@gmail.com>
2023-12-27 16:19:49 +0700

    fix: page content padding

--- packages/theme/src/components/page-content.ts
+++ packages/theme/src/components/page-content.ts
@@ -3,7 +3,7 @@ import { clsx } from 'clsx'
 const pageContentWrapperClsx = ({
   className = '',
 }: { className?: string } = {}) =>
-  clsx('px-4 py-8 lg:p-8 min-[1440px]:p-14 overflow-y-auto grow', className)
+  clsx('p-4 lg:p-8 min-[1440px]:p-14 overflow-y-auto grow', className)
 
 const pageContentClsx = ({ className = '' }: { className?: string } = {}) =>
   clsx('max-w-[952px] mx-auto', className)
