74475713e98f1840099afb9d48fb4799518dba37
Chinh Le <chinhle.120393@gmail.com>
2023-12-28 14:36:20 +0700

    chore: tailwinds storybook

--- packages/storybook/tailwind.config.js
+++ packages/storybook/tailwind.config.js
@@ -2,6 +2,7 @@ import { mochiui } from '@mochi-ui/theme'
 
 module.exports = {
   content: [
+    '../web3/*/stories/**/*.stories.{js,jsx,ts,tsx}',
     '../components/*/stories/**/*.stories.{js,jsx,ts,tsx}',
     '../theme/src/components/**/*.{js,jsx,ts,tsx}',
   ],
