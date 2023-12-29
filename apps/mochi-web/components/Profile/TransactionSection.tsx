904cbc83dcedf466859424e4e47f00e4b04cc3d4
Thanh Pham <zlatan.pham@gmail.com>
2023-12-29 16:31:05 +0700

    fix(mochi-web): remove redirect click on transaction table

--- apps/mochi-web/components/Profile/TransactionSection.tsx
+++ apps/mochi-web/components/Profile/TransactionSection.tsx
@@ -6,23 +6,17 @@ import Transaction from '~cpn/Transaction'
 
 export const TransactionSection = () => {
   return (
-    <div>
-      <Link href={ROUTES.TRANSACTIONS}>
-        <div className="flex flex-col rounded-lg border bg-background-surface border-divider">
-          <div className="flex items-center p-4 space-x-4">
-            <DocumentOneSolid className="w-6 h-6" />
-            <Typography level="h8" className="flex-1">
-              Transactions
-            </Typography>
-            <ArrowRightLine className="w-6 h-6" />
-          </div>
-          <Transaction
-            filterType="all"
-            filterPlatform="all"
-            defaultPageSize={4}
-          />
-        </div>
-      </Link>
+    <div className="flex flex-col rounded-lg border bg-background-surface border-divider">
+      <div className="flex items-center p-4 space-x-4">
+        <DocumentOneSolid className="w-6 h-6" />
+        <Typography level="h8" className="flex-1">
+          Transactions
+        </Typography>
+        <Link href={ROUTES.TRANSACTIONS}>
+          <ArrowRightLine className="w-6 h-6" />
+        </Link>
+      </div>
+      <Transaction filterType="all" filterPlatform="all" defaultPageSize={4} />
     </div>
   )
 }
