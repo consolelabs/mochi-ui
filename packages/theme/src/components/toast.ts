ac6ed1971d496a71c0bee1270340d334408a7c3c
Bui Ngoc Nam Anh <88917321+tonible14012002@users.noreply.github.com>
2024-01-05 15:24:20 +0700

    feat: animation for toast (#575)
    * feat: animation for toast
    
    * fix: update animation to prevent overflow container

--- packages/theme/src/components/toast.ts
+++ packages/theme/src/components/toast.ts
@@ -1,16 +1,29 @@
 import { VariantProps, cva } from 'class-variance-authority'
 
-const toastCva = cva('w-fit pointer-events-auto', {
-  variants: {
-    fullWidth: {
-      true: 'w-full',
-      false: 'w-fit',
+const toastCva = cva(
+  [
+    'w-fit pointer-events-auto',
+    'data-[state=open]:animate-in',
+    'data-[state=open]:zoom-in-95',
+    'data-[state=closed]:fade-in-50',
+    'data-[state=closed]:animate-out',
+    'data-[state=closed]:zoom-out-95',
+    'data-[state=closed]:fade-out-0',
+    'transition-all',
+    'duration-200',
+  ],
+  {
+    variants: {
+      fullWidth: {
+        true: 'w-full',
+        false: 'w-fit',
+      },
+    },
+    defaultVariants: {
+      fullWidth: false,
     },
   },
-  defaultVariants: {
-    fullWidth: false,
-  },
-})
+)
 
 const toastViewPortCva = cva(
   [
