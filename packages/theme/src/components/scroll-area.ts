6ac1d224b95f45dc8e6ae86fbcda22bb625f96dc
tonible14012002 <namanh14.work@gmail.com>
2024-01-08 12:12:49 +0700

    fix: update color scrollbar

--- packages/theme/src/components/scroll-area.ts
+++ packages/theme/src/components/scroll-area.ts
@@ -12,21 +12,20 @@ const scrollAreaScrollbarClsx = ({
   orientation = 'vertical',
 }: { className?: string; orientation?: 'horizontal' | 'vertical' } = {}) =>
   clsx(
-    'flex select-none touch-none p-0.5 bg-background-level2 transition-colors hover:bg-background-level3',
+    'flex select-none touch-none p-0.5 bg-transparent transition-colors',
     orientation === 'vertical' ? 'w-2' : 'h-2 flex-col',
     className,
   )
 
 const scrollAreaThumbClsx = ({ className = '' }: { className?: string } = {}) =>
   clsx(
-    'flex-1 bg-text-primary rounded-lg relative before:content-[""] before:absolute before:w-full before:h-full before:min-w-[44px] before:min-h-[44px] before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2',
+    'flex-1 bg-text-primary opacity-80 hover:opacity-100 rounded-lg relative before:content-[""] before:absolute before:w-full before:h-full before:min-w-[44px] before:min-h-[44px] before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2',
     className,
   )
 
 const scrollAreaCornerClsx = ({
   className = '',
-}: { className?: string } = {}) =>
-  clsx('bg-background-level3 !bg-black', className)
+}: { className?: string } = {}) => clsx('bg-transparent !bg-black', className)
 
 const scrollArea = {
   scrollAreaRootClsx,
