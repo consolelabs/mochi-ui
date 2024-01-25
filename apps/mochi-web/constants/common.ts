bb8d303176d473f9eefa638cea495dbc4855a281
tonible14012002 <namanh14.work@gmail.com>
2024-01-25 18:24:33 +0700

    feat: add social sharing

--- apps/mochi-web/constants/common.ts
+++ apps/mochi-web/constants/common.ts
@@ -1,3 +1,16 @@
 import packageJson from 'package.json'
 
 export const appVersion = packageJson.version
+
+export const SHARE_CONTENT = 'Check out the latest update of Mochi.gg 👇'
+
+export const FACEBOOK_SHARE_URL = (url: string) =>
+  `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`
+
+export const TWITTER_SHARE_URL = (url: string, content: string) =>
+  `http://twitter.com/share?url=${encodeURIComponent(
+    url,
+  )}&text=${encodeURIComponent(content)}`
+
+export const MAIL_SHARE_URL = (subject: string, body: string) =>
+  `mailto:?subject=${subject}&body=${encodeURIComponent(body)}`
