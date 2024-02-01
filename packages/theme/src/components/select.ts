428ca4bfd24994c651cd7021e53c6313dfe9f18b
Thanh Pham <zlatan.pham@gmail.com>
2024-02-01 18:23:49 +0700

    fix: update animation

--- packages/theme/src/components/select.ts
+++ packages/theme/src/components/select.ts
@@ -19,23 +19,23 @@ const content = [
 
   'data-[state=open]:animate-in',
   'data-[state=open]:fade-in-0',
-  'data-[state=open]:zoom-in-0',
+  'data-[state=open]:zoom-in-50',
   'data-[state=open]:duration-300',
 
-  'data-[state=open]:data-[side=bottom]:slide-in-from-top-1/2',
-  'data-[state=open]:data-[side=left]:slide-in-from-right-1/2',
-  'data-[state=open]:data-[side=right]:slide-in-from-left-1/2',
-  'data-[state=open]:data-[side=top]:slide-in-from-bottom-1/2',
+  'data-[state=open]:data-[side=bottom]:slide-in-from-top-10',
+  'data-[state=open]:data-[side=left]:slide-in-from-right-10',
+  'data-[state=open]:data-[side=right]:slide-in-from-left-10',
+  'data-[state=open]:data-[side=top]:slide-in-from-bottom-10',
 
   'data-[state=closed]:animate-out',
   'data-[state=closed]:fade-out-0',
-  'data-[state=closed]:zoom-out-0',
+  'data-[state=closed]:zoom-out-50',
   'data-[state=closed]:duration-300',
 
-  'data-[state=closed]:data-[side=bottom]:slide-out-to-top-1/2',
-  'data-[state=closed]:data-[side=left]:slide-out-to-right-1/2',
-  'data-[state=closed]:data-[side=right]:slide-out-to-left-1/2',
-  'data-[state=closed]:data-[side=top]:slide-out-to-bottom-1/2',
+  'data-[state=closed]:data-[side=bottom]:slide-out-to-top-10',
+  'data-[state=closed]:data-[side=left]:slide-out-to-right-10',
+  'data-[state=closed]:data-[side=right]:slide-out-to-left-10',
+  'data-[state=closed]:data-[side=top]:slide-out-to-bottom-10',
 ].join(' ')
 
 const viewportPoperMode =
