67309d73f0615d89e0e295b5cba010b8f423dc66
Hien Le <44285614+leduyhien152@users.noreply.github.com>
2024-02-01 18:37:23 +0700

    refactor: dropdown and select animation (#710)
    * refactor: dropdown and select animation
    
    * fix: update animation
    
    ---------
    
    Co-authored-by: Thanh Pham <zlatan.pham@gmail.com>

--- packages/theme/src/components/dropdown.ts
+++ packages/theme/src/components/dropdown.ts
@@ -4,16 +4,23 @@ import { cva } from 'class-variance-authority'
 const contentAnimations = [
   'data-[state=open]:animate-in',
   'data-[state=open]:fade-in-0',
-  'data-[state=open]:zoom-in-95',
+  'data-[state=open]:zoom-in-50',
+  'data-[state=open]:duration-300',
 
-  'data-[side=bottom]:slide-in-from-top-2',
-  'data-[side=left]:slide-in-from-right-2',
-  'data-[side=right]:slide-in-from-left-2',
-  'data-[side=top]:slide-in-from-bottom-2',
+  'data-[state=open]:data-[side=bottom]:slide-in-from-top-10',
+  'data-[state=open]:data-[side=left]:slide-in-from-right-10',
+  'data-[state=open]:data-[side=right]:slide-in-from-left-10',
+  'data-[state=open]:data-[side=top]:slide-in-from-bottom-10',
 
   'data-[state=closed]:animate-out',
   'data-[state=closed]:fade-out-0',
-  'data-[state=closed]:zoom-out-95',
+  'data-[state=closed]:zoom-out-50',
+  'data-[state=closed]:duration-300',
+
+  'data-[state=closed]:data-[side=bottom]:slide-out-to-top-10',
+  'data-[state=closed]:data-[side=left]:slide-out-to-right-10',
+  'data-[state=closed]:data-[side=right]:slide-out-to-left-10',
+  'data-[state=closed]:data-[side=top]:slide-out-to-bottom-10',
 ]
 
 const dropdownIconStyleCva = cva(
