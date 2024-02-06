09843ecf24b54fcf56b0eb9c8326989d28cdb0e9
tuanddd <vincenttuandao@gmail.com>
2024-02-06 11:29:16 +0700

    fix: tweak

--- apps/mochi-web/app/layout/seo.tsx
+++ apps/mochi-web/app/layout/seo.tsx
@@ -40,5 +40,11 @@ export const SEO = ({
     <meta property="twitter:title" content={title} />
     <meta property="twitter:description" content={description} />
     <meta property="twitter:image" content={image || CONFIG.image} />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="manifest" href="/site.webmanifest" />
   </Head>
 )
