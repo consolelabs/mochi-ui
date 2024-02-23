1a8b9a4476d9757d361e6bc3a7756d3d412df5b9
tonible14012002 <namanh14.work@gmail.com>
2024-02-23 12:00:48 +0700

    fix: hydration error useTheme

--- apps/mochi-web/hooks/useTheme.ts
+++ apps/mochi-web/hooks/useTheme.ts
@@ -1,10 +1,25 @@
 import { useTheme as useInternalTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+
+export type ActiveThemeValue = 'light' | 'dark' | undefined
+export type ThemeValue = 'light' | 'dark' | 'system' | undefined
 
 export const useTheme = () => {
   const { theme, systemTheme, ...rest } = useInternalTheme()
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  // Fix Hydration Error
+  let activeTheme: string | undefined = 'light'
+  if (mounted) {
+    activeTheme = theme === 'system' ? systemTheme : (theme as ThemeValue)
+  }
+
   return {
-    activeTheme: theme === 'system' ? systemTheme : theme,
-    theme,
+    activeTheme: activeTheme as ActiveThemeValue,
+    theme: theme as ThemeValue,
     systemTheme,
     ...rest,
   }
